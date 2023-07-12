e9597ab724c14418d705879f9d9609068ff507fe
sanjoso403 <sanjoso403@gmail.com>
2023-07-12 16:13:28 -0400

    Worked on put request for users. Needs testing

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,20 +5,28 @@ const usersRouter = require("express").Router();
 const pool = require("../db");
 const verifyToken = require("../utils/verifyToken");
 
+// Verifies token
+usersRouter.use("/", async (req, res, next) => {
+	const validToken = await verifyToken(req.query.token);
+	if (validToken.status !== 200) {
+		res.status(401).send("Authentication failed.");
+	}
+	next();
+});
+
 // Gets user's info
 usersRouter.get("/:userName", async (req, res) => {
 	const { userName } = req.params;
-	const validToken = await verifyToken(req.query.token);
-	console.log(validToken);
+	//const validToken = await verifyToken(req.query.token);
 	try {
-		if (validToken.status === 200) {
-			const userInfo = await pool.query(
-				"SELECT * FROM users WHERE username = $1",
-				[userName]
-			);
+		const userInfo = await pool.query(
+			"SELECT * FROM users WHERE username = $1",
+			[userName]
+		);
+		res.status(200).json(userInfo);
+		// if (validToken.status === 200) {
 
-			res.status(200).json(userInfo);
-		}
+		// }
 	} catch (err) {
 		res.status(401).send("Authentication failed");
 	}
@@ -26,6 +34,39 @@ usersRouter.get("/:userName", async (req, res) => {
 
 //Updates user's info
 
-usersRouter.put("/:userName", async (req, res) => {});
+usersRouter.put("/:userName", async (req, res) => {
+	const {
+		userName,
+		firstName,
+		lastName,
+		emailAddress,
+		phoneNumber,
+		streetAddress,
+		city,
+		state,
+		zipcode,
+	} = req.body;
+
+	try {
+		const updatedInfo = pool.query(
+			"UPDATE users SET  first_name = $2, last_name = $3, email_address = $4, phone_number = $5, street_address = $6, city = $7, state = $8, zipcode = $9 WHERE username = $1",
+			[
+				userName,
+				firstName,
+				lastName,
+				emailAddress,
+				phoneNumber,
+				streetAddress,
+				city,
+				state,
+				zipcode,
+			]
+		);
+		res.status(204).json(updatedInfo);
+	} catch (err) {
+		console.log(err);
+		res.status(400).send("");
+	}
+});
 
 module.exports = usersRouter;
