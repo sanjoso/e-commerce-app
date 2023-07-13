5a5da9209719575bedd1f9dd230f2b4fbfcb27e0
sanjoso403 <sanjoso403@gmail.com>
2023-07-13 17:24:30 -0400

    Works on product urls. Figure out jwt verify spot

---
+++ server/routes/products.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const productsRouter = require("express").Router();
+const pool = require("../db");
+
+// Get a specific product
+productsRouter.get("/productId", async (req, res) => {
+	const { productId } = req.params;
+	try {
+		const foundProduct = await pool.query(
+			"SELECT * FROM product WHERE product_id = $1",
+			[productId]
+		);
+		if (!foundProduct.rows) {
+			res.status(404).send("Product doens't exist");
+		}
+		res.status(200).json(foundProduct);
+	} catch (err) {
+		console.log(err);
+	}
+});
+
+// Get products by category
+productsRouter.get(":category", async (req, res) => {
+	const { categoryId } = req.query;
+	try {
+		const products = await pool.query(
+			"SELECT * FROM product WHERE category = $1",
+			[categoryId]
+		);
+		if (!products.rows) {
+			res.status(404).send("Category does not exist");
+		}
+		res.status(200).json(products);
+	} catch (err) {
+		console.log(err);
+	}
+});
+
+module.exports = productsRouter;
--- server/server.js
+++ server/server.js
@@ -4,10 +4,14 @@ const PORT = process.env.PORT || 8000;
 
 const authRouter = require("./routes/auth");
 const usersRouter = require("./routes/users");
+const productsRouter = require("./routes/products");
+const ordersRouter = require("./routes/orders");
 
 app.use(express.json());
 app.use("/auth", authRouter);
 app.use("/users", usersRouter);
+app.use("/products", productsRouter);
+app.use("/orders", ordersRouter);
 
 app.listen(PORT, () => {
 	console.log(`Server started on port ${PORT}`);
