fd5020b18b7c7e72894a6a6cdc52c31fcce46895
sanjoso403 <sanjoso403@gmail.com>
2023-07-16 22:32:36 -0400

    Works on cart endpoints. Need URL schema improved

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cartRouter = require("express").Router();
+const { v4: uuidv4 } = require("uuid");
 const pool = require("../db");
 const verifyToken = require("../utils/verifyToken");
 
@@ -9,14 +10,15 @@ cartRouter.use("/", verifyToken, async (req, res, next) => {
 });
 
 // Gets the cart id
-cartRouter.use("/", async (req, res, next) => {
-	const { username } = req.body;
+cartRouter.param("username", async (req, res, next) => {
+	const { username } = req.params;
 	try {
 		const cartId = await pool.query(
 			"SELECT user_id FROM users WHERE username = $1",
 			[username]
 		);
-		req.cartId = cartId;
+		req.cartId = cartId.rows[0].user_id;
+		//console.log(cartId.rows[0].user_id);
 		next();
 	} catch (err) {
 		console.log(err);
@@ -24,8 +26,10 @@ cartRouter.use("/", async (req, res, next) => {
 });
 
 // Gets the items in a user's cart
-cartRouter.get("/:cartId", async (req, res) => {
+cartRouter.get("/:username", async (req, res) => {
 	const { cartId } = req.params;
+	console.log("here");
+	console.log(req.cartId);
 	try {
 		const cartItems = await pool.query(
 			"SELECT * FROM cart WHERE cart_id = $1",
@@ -54,6 +58,45 @@ cartRouter.post("/:cartId", async (req, res) => {
 			res.status(500).send("Server error. Please try again.");
 		}
 		res.status(200).send("Item added successfully.");
+	} catch (err) {
+		console.log(err);
+	}
+});
+
+// Checkout endpoint that creates an order
+cartRouter.post("/:cartId/checkout", async (req, res) => {
+	const {
+		cartId,
+		shiptoStreet,
+		shiptoCity,
+		shiptoState,
+		shiptoZipcode,
+		cardNumber,
+		expirationDate,
+		CVV,
+	} = req.body;
+
+	try {
+		// First, check that cart exists
+		const cartExists = await pool.query(
+			"SELECT * FROM cart where cart_id = $1",
+			[cartId]
+		);
+		if (!cartExists) {
+			res.status(204).send("Cart is empty.");
+		}
+		//Then, charge the card
+		const total = "SELECT SUM(price) AS total FROM cart WHERE cart_id = x"; //FIND THE TOTAL FROM ADDING THE PRICES IN THE DB
+		const chargeCard = true;
+		if (!chargeCard) {
+			res.status(400).send("Payment not successful.");
+		}
+		//Then, create an order in the database
+		const orderId = uuidv4();
+		const orderCreated = await pool.query(
+			"INSERT INTO orders (order_id, date, total, shipto_street, shipto_city, shipto_state, shipto_zipcode, email, pay_method, card_num, user_id VALUES $1,$2, $3, $4, $5, $6, $7, $8, $9, $10, $11",
+			[orderId, Date.now()]
+		);
 	} catch (err) {
 		console.log(err);
 	}
